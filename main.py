fb7f2d844a97c7d58f8eb39a697c9c8d50800763
MingrenChen <chenmr9769@gmail.com>
2018-01-02 22:48:04 +0800

    sending emails for remainder

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import json
+import smtplib
+from email.mime.text import MIMEText
+
 
 driver = webdriver.Chrome()
+# driver = webdriver.Remote("http://localhost:4444/wd/hub", webdriver.DesiredCapabilities.HTMLUNITWITHJS)
 driver.get("http://www.acorn.utoronto.ca/")
 login = driver.find_element_by_xpath("/html/body/div[2]/div/div[3]/div/div/div[2]/p[2]/a").click()
+print("able to enter username/pwd")
 username = driver.find_element_by_xpath("//*[@id=\"inputID\"]")
 username.clear()
 username.send_keys("chenmi84")
@@ -12,15 +18,68 @@ pwd.clear()
 pwd.send_keys("Harry6966xx511")
 submit = driver.find_element_by_xpath("//*[@id=\"query\"]/button")
 submit.click()
-submit.click()
+print("login")
+try:
+    submit.click()
+except:
+    pass
 
 academic = driver.get("https://acorn.utoronto.ca/sws/transcript/academic/main.do?main.dispatch")
 content = driver.page_source
 
-soup = BeautifulSoup(content)
-print(soup)
-#
+soup = BeautifulSoup(content,"lxml")
+lst1 = soup.find(class_ = "academic-history-recent").find_all(class_ = "courses")
 
+def deletet(string):
+    new_str = ''
+    for i in string:
+        if not i == '\n' and '\t':
+            new_str += i
+    if new_str != '':
+        return int(new_str)
+    return None
+
+
+mark = {}
+for course in lst1:
+    mark[course.find('td').string] = deletet(course.find(class_ = 'course-mark').string)
+
+last_ = None
+try:
+    with open("grade.json", 'r') as data:
+        last_ = json.load(data)
+except:
+    print("here")
+    json.dump(mark, open("grade.json", 'w'))
+    with open("grade.json", 'r') as data:
+        last_ = json.load(data)
+
+
+msg = ""
+if last_ != mark:
+    print("there's something different")
+    for i in mark:
+        if mark[i] != last_[i]:
+            if last_[i] is None:
+                msg += "{}'s mark is out, you got {}\n".format(i, mark[i])
+            else:
+                msg += "{}'s mark is change, you get {} now\n".format(i, mark[i])
+        else:
+            print(i, "is the same")
+    json.dump(mark, open("grade.json", 'w'))
+else:
+    print("everything keeps the same")
+
+msg = MIMEText(msg)
+me = 'autoRemainder'
+you = 'chenmr9769@gmail.com'
+msg['Subject'] = 'Your grade has been changed.'
+msg['From'] = you
+msg['To'] = you
+
+s = smtplib.SMTP('localhost')
+s.sendmail(you, [you], msg.as_string())
+s.quit()
 
 
 
