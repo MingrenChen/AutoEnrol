7f10e3fbccd0159463bf335241ba14c954eca529
Mingren <chenmr9769@gmail.com>
2018-01-04 03:42:09 +0000

    s

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import json
 import smtplib
 from email.mime.text import MIMEText
 from datetime import datetime
+from pytz import timezone
 
 log = open("log.txt", "a")
 # driver = webdriver.Chrome()
 def renew_grade():
-    log.write("="*30 + '\n')
-    log.write("operation start at {}\n\n".format(datetime.now()))
+    log.write("\n"+"="*30 + '\n')
+    log.write("operation start at {}\n\n".format(datetime.now(timezone('UTC')).astimezone(timezone('US/Eastern'))))
     driver = webdriver.Remote("http://localhost:4444/wd/hub", webdriver.DesiredCapabilities.HTMLUNITWITHJS)
     driver.get("http://www.acorn.utoronto.ca/")
     driver.find_element_by_xpath("/html/body/div[2]/div/div[3]/div/div/div[2]/p[2]/a").click()
-    log.write("able to enter username/pwd")
+    log.write("able to enter username/pwd\n")
     username = driver.find_element_by_xpath("//*[@id=\"inputID\"]")
     username.clear()
     username.send_keys("chenmi84")
@@ -22,7 +23,7 @@ def renew_grade():
     pwd.send_keys("Harry6966xx511")
     submit = driver.find_element_by_xpath("//*[@id=\"query\"]/button")
     submit.click()
-    log.write("login")
+    log.write("login\n")
     try:
         submit.click()
     except:
@@ -58,7 +59,7 @@ def renew_grade():
 
     msg = ""
     if last_ != mark:
-        log.write("there's something different")
+        log.write("there's something different\n")
         for i in mark:
             if mark[i] != last_[i]:
                 if last_[i] is None:
@@ -66,21 +67,22 @@ def renew_grade():
                 else:
                     msg += "{}'s mark is change, you get {} now\n".format(i, mark[i])
             else:
-                log.write("{} is the same".format(i))
+                log.write("{} is the same\n".format(i))
         json.dump(mark, open("grade.json", 'w'))
     else:
-        log.write("everything keeps the same")
+        log.write("everything keeps the same\n")
 
     if msg != '':
         msg = MIMEText(msg)
         me = 'autoRemainder@aspada.life'
         you = 'chenmr9769@gmail.com'
+        you2 = 'mingren_chen@outlook.com'
         msg['Subject'] = 'Your grade has been changed.'
         msg['From'] = me
         msg['To'] = you
 
         s = smtplib.SMTP('localhost')
-        s.sendmail(me, [you], msg.as_string())
+        s.sendmail(me, [you, you2], msg.as_string())
         s.quit()
-        log.write("email has been send")
+        log.write("email has been send\n")
 renew_grade()
